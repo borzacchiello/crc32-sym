f87d5ed74b619617e782069f8eb3b0166f9ca0ee
Luca Borzacchiello <lucaborza@gmail.com>
2021-07-27 16:28:21 +0200

    seninja exp concretization mode

--- bninja_scripts/harness.py
+++ bninja_scripts/harness.py
@@ -15,6 +15,20 @@ def run_one(bv, size):
     seninja.reset_se()
     return t, f
 
+def run_one_concretize(bv, size):
+    seninja.start_se(bv, 0x4006c0)
+
+    executor = seninja.get_executor()
+    executor.bncache.settings["memory.symb_address_mode"] = "concretization"
+
+    seninja.setup_argv(
+        seninja.str_to_bv(str(size), terminator=True))
+
+    _ = seninja.continue_until_address(0x4006b0)
+    print(executor.fringe)
+
+    seninja.reset_se()
+
 def run(bv):
     fout = open(os.path.join(SCRIPTDIR, "../results/seninja_data.csv"), "w")
     size = 1
@@ -27,6 +41,22 @@ def run(bv):
         print("[+] evaluating for size %d" % size)
         buf = f.solver.evaluate(seninja.get_stdin_bv(f)).as_bytes()
         fout.write("%s, %f, %#x\n" % (size, elapsed, zlib.crc32(buf)))
+        fout.flush()
+
+        size *= 2
+
+    fout.close()
+
+def run_conc(bv):
+    fout = open(os.path.join(SCRIPTDIR, "../results/seninja_data_conc.csv"), "w")
+    size = 1
+    while size <= 1024:
+        print("[+] running size %d" % size)
+        start   = time.time()
+        run_one_concretize(bv, size)
+        elapsed = time.time() - start
+
+        fout.write("%s, %f\n" % (size, elapsed))
         fout.flush()
 
         size *= 2
